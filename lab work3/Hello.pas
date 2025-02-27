5bcf75de3c07f0fabb53816773f3ebedcc9f9df5
Shyxxx234 <maxkott234@gmail.com>
2025-02-27 14:00:22 +0300

    Solve problem with name of parametr

--- lab work3/Hello.pas
+++ lab work3/Hello.pas
@@ -2,19 +2,24 @@ PROGRAM Hello(INPUT, OUTPUT);
 USES
   DOS;
 VAR
-  FirstName, Str: STRING;
+  Name, Str: STRING;
 BEGIN
+  Name := '';
   WRITELN('Content-Type: text/plain');
   WRITELN;
-  Str := GetEnv('QUERY_STRING'); 
-  DELETE(Str, 1, POS('name=', Str));
-  DELETE(Str, 1, POS('=', Str));
-  FirstName := Copy(Str, 1, POS('&', Str) - 1);
-  IF FirstName = ''
+  Str := GetEnv('QUERY_STRING');
+  IF POS('name=', Str) <> 0
+  THEN
+    BEGIN 
+      DELETE(Str, 1, POS('name=', Str));
+      DELETE(Str, 1, POS('=', Str));
+      Name := Copy(Str, 1, POS('&', Str) - 1);
+    END;                          
+  IF Name = ''
   THEN
     WRITELN('Hello Anonymous!')    
   ELSE
-    WRITELN('Hello dear, ', FirstName, '!')
+    WRITELN('Hello dear, ', Name, '!')
 END. 
 
 {http://localhost/cgi-bin/hello.cgi?name=Max}
--- lab work3/WorkWithQueryString.pas
+++ lab work3/WorkWithQueryString.pas
@@ -5,10 +5,16 @@ FUNCTION GetQueryStringParameter(Key: STRING): STRING;
 VAR
   Str: STRING;
 BEGIN
-  Str := GetEnv('QUERY_STRING') + ','; 
-  DELETE(Str, 1, POS(Key, Str));
-  DELETE(Str, 1, POS('=', Str));
-  GetQueryStringParameter := Copy(Str, 1, POS(',', Str) - 1)
+  Str := GetEnv('QUERY_STRING') + '&'; 
+  IF POS(Key + '=', Str) <> 0
+  THEN
+    BEGIN
+      DELETE(Str, 1, POS(Key + '=', Str));
+      DELETE(Str, 1, POS('=', Str));
+      GetQueryStringParameter := Copy(Str, 1, POS('&', Str) - 1)
+    END
+  ELSE
+    GetQueryStringParameter := ''
 END;
 BEGIN {WorkWithQueryString}
   WRITELN('Content-Type: text/plain');
@@ -17,5 +23,3 @@ BEGIN {WorkWithQueryString}
   WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
   WRITELN('Age: ', GetQueryStringParameter('age'))
 END. {WorkWithQueryString}
-
-{http://localhost/cgi-bin/workwithquerystring.cgi?first_name=Max,%20last_name=Sayfulin,%20age=18}
