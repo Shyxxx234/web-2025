ae149aac05bc3f42c57c5c7aeb21447ce250c76e
Shyxxx234 <maxkott234@gmail.com>
2025-03-27 13:05:48 +0300

    rewrite program to case

--- Project/DigitToWord.php
+++ Project/DigitToWord.php
@@ -1,28 +1,40 @@
 <?php
 if (isset($_GET['digit'])) {
     $digit = $_GET['digit'];
-    if ($digit == 0) {
-        echo "<p>Ноль</p>";
-    } elseif ($digit == 1) {
-        echo "<p>Один</p>";
-    } elseif ($digit == 2) {
-        echo "<p>Два</p>";
-    } elseif ($digit == 3) {
-        echo "<p>Три</p>";
-    } elseif ($digit == 4) {
-        echo "<p>Четыре</p>";
-    } elseif ($digit == 5) {
-        echo "<p>Пять</p>";
-    } elseif ($digit == 6) {
-        echo "<p>Шесть</p>";
-    } elseif ($digit == 7) {
-        echo "<p>Семь</p>";
-    } elseif ($digit == 8) {
-        echo "<p>Восемь</p>";
-    } elseif ($digit == 9) {
-        echo "<p>Девять</p>";
-    } else {
-        echo "<p>Введите цифру</p>";
+    switch ($digit) {
+        case 0:
+            echo "<p>Ноль</p>";
+            break;  
+        case 1:
+            echo "<p>Один</p>";
+            break;
+        case 2:
+            echo "<p>Два</p>";
+            break;
+        case 3:
+            echo "<p>Три</p>";
+            break;
+        case 4:
+            echo "<p>Четыре</p>";
+            break;
+        case 5:
+            echo "<p>Пять</p>";
+            break;
+        case 6:
+            echo "<p>Шесть</p>";
+            break;
+        case 7:
+            echo "<p>Семь</p>";
+            break;
+        case 8:
+            echo "<p>Восемь</p>";
+            break;
+        case 9:
+            echo "<p>Девять</p>";
+            break;
+        default:
+            echo "<p>Введите цифру</p>";
+            break;
     }
 }
 ?>
